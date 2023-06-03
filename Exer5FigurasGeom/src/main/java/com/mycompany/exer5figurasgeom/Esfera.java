b4e71fa2c81255bfb7cddfaff805a6ec5cdfdb00
dramirezla <53583408+dramirezla@users.noreply.github.com>
2023-06-03 07:07:31 -0500

    Add files via upload

---
+++ Exer5FigurasGeom/src/main/java/com/mycompany/exer5figurasgeom/Esfera.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exer5figurasgeom;
+
+/**
+ *
+ * @author David R.L
+ */
+public class Esfera extends FiguraGeometrica{
+    private double radio; // Atributo que identifica el radio de una esfera
+/**
+* Constructor de la clase Esfera
+* @param radio Parámetro de define el radio de una esfera
+500 Ejercicios de programación orientada a objetos con Java y UML
+*/
+public Esfera(double radio) {
+this.radio = radio;
+this.setVolumen(calcularVolumen()); /* Calcula el volumen y 
+establece su atributo */
+this.setSuperficie(calcularSuperficie()); /* Calcula la superficie y 
+establece su atributo */
+}
+/**
+* Método para calcular el volumen de una esfera
+* @return El volumen de una esfera
+*/
+public double calcularVolumen() {
+double volumen = 1.333 * Math.PI * Math.pow(this.radio, 3.0);
+return volumen;
+}
+/**
+* Método para calcular la superficie de una esfera
+* @return La superficie de una esfera
+*/
+public double calcularSuperficie() {
+double superficie = 4.0 * Math.PI * Math.pow(this.radio, 2.0);
+return superficie;
+}
+}
---
+++ Exer5FigurasGeom/src/main/java/com/mycompany/exer5figurasgeom/Exer5FigurasGeom.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exer5figurasgeom;
+
+/**
+ *
+ * @author David R.L
+ */
+public class Exer5FigurasGeom {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; /* Define la ventana 
+principal */
+miVentanaPrincipal= new VentanaPrincipal(); /* Crea la ventana 
+principal */
+miVentanaPrincipal.setVisible(true); /* Establece la ventana 
+como visible */
+// Establece que la ventana no puede cambiar su tamaño
+miVentanaPrincipal.setResizable(false);
+    }
+}
