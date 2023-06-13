6ac23ca634ecf26ccac27c2aa136e7000c3878e5
Sevillegas <sevillegas@unal.edu.co>
2023-06-13 18:16:08 -0500

    EjercicioNotas
    Actividad # 5
    Programación Orientada a Objetos
    Estudiante: Sebastian Villegas Múnera
    Profeso: Walter Arboleda

---
+++ EjercicioNotas/src/main/java/Notas/Principal.java
@@ -0,0 +1,13 @@
+
+
+package Notas;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+         VentanaPrincipal miVentanaPrincipal;
+        miVentanaPrincipal= new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+    }
+}
